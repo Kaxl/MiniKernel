4a7d8d3ad3d64e9aa6ed4c66448e5437f79e73d5
Axel Fahy <axel@fahy.net>
2015-10-16 12:49:57 +0200

    Add some comment in base.h and add swp files in gitignore

--- common/base.h
+++ common/base.h
@@ -34,19 +34,26 @@ void *memset(void *dst, int value, uint count);
 /**
  * @brief Copy memory area
  *
+ * The  memcpy() function copies count bytes from memory area src to memory area dst.
+ * The memory areas must not overlap.
+ *
  * @param dst   Destination
  * @param src   Source
- * @param count 
+ * @param count Number of bytes
  *
  * @return
  */
 void *memcpy(void *dst, void *src, uint count);
 
 /**
- * @brief Compares two strings.
+ * @brief Compares two strings
  *
- * @param p String one
- * @param q String two
+ * The  strcmp() function compares the two strings s1 and s2.
+ * It returns an integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
+ *
+ * @param p String one (s1)
+ * @param q String two (s2)
  * @param n Number of bytes
  *
  * @return int (-1, 0, 1) following the size of the first string and the second one
