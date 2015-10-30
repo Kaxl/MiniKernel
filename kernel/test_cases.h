7c21c610c3721458be96b4c2ef4d3d7f3de5beb3
Axel Fahy <axel@fahy.net>
2015-10-30 16:33:39 +0100

    Add test_cases.h

---
+++ kernel/test_cases.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test_cases.h
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  30/16/2015 11:22:12 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Axel Fahy, Rudolf Hohn
+ *   Organization:  HES-SO hepia section ITI
+ *
+ * =====================================================================================
+ */
+
+#ifndef _TEST_CASES_H_
+#define _TEST_CASES_H_
+
+/**
+ * @brief Test of the kernel
+ *
+ * Test kernel functionnalities.
+ */
+void runKernelTest();
+
+#endif
