b396ec48ff8daa005d9171e98bb98ae877cf3740
Axel Fahy <axel@fahy.net>
2015-11-24 16:05:46 +0100

    Add comments for timer

--- kernel/timer.c
+++ kernel/timer.c
@@ -7,6 +7,7 @@ volatile timer t = {
     .freq_hz = 0
 };
 
+////////////////////////////////////////////////////////////////////////////////////////
 void timer_init(uint32_t freq_hz) {
     uint16_t div = 0;
     // Check the frequency
@@ -26,28 +27,18 @@ void timer_init(uint32_t freq_hz) {
     t.freq_hz = freq_hz;
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
 void timer_handler() {
     // wait for tick
     t.tick++;
 }
 
-/**
- * @brief Get the number of tick since the system started.
- *
- * @return  The tick's number
- */
+////////////////////////////////////////////////////////////////////////////////////////
 uint get_ticks() {
     return t.tick;
 }
 
-/**
- * @brief Wait for 'ms' milliseconds
- *
- * Calculate last tick of the sleep and wait while the current
- * tick has not reached it.
- *
- * @param ms Number of milliseconds to wait
- */
+////////////////////////////////////////////////////////////////////////////////////////
 void sleep(uint ms) {
     uint32_t end = get_ticks() + (ms / 1000 * t.freq_hz);
 
--- kernel/timer.h
+++ kernel/timer.h
@@ -17,9 +17,35 @@ typedef struct timer {
     uint32_t freq_hz;
 } timer;
 
+/**
+ * @brief Initialization of timer
+ *
+ * @param freq_hz Frequency of the timer (~100hz on most system)
+ */
 extern void timer_init(uint32_t freq_hz);
+
+/**
+ * @brief Increment the timer value.
+ *
+ * Called by the timer interruption.
+ */
 extern void timer_handler();
+
+/**
+ * @brief Get the number of tick since the system started.
+ *
+ * @return  The tick's number
+ */
 extern uint get_ticks();
+
+/**
+ * @brief Wait for 'ms' milliseconds
+ *
+ * Calculate last tick of the sleep and wait while the current
+ * tick has not reached it.
+ *
+ * @param ms Number of milliseconds to wait
+ */
 extern void sleep(uint ms);
 
 #endif
