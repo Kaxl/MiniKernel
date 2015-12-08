71c742ee9970e142f338f9ec252694b1063e68bf
Rudolf Hohn <rudolf.hohn@etu.hesge.ch>
2015-12-08 16:07:15 +0100

    Debuging of all pointer of structures of pointers of structures of pointers of ... Ok i'm done

--- tools/pfs.c
+++ tools/pfs.c
@@ -19,15 +19,16 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "const.h"
 #include "pfs.h"
 
 //========================================================
 // Local functions to load all PFS data
 int loadSuperblock(superblock_t* superblock, FILE* image);
 
-int loadBitmap(unsigned char* bitmap, FILE* image);
+int loadBitmap(unsigned char* bitmap, FILE* image, superblock_t* superblock);
 
-int loadFileEntries(file_entry_t* fileEntries, FILE* image);
+int loadFileEntries(file_entry_t* fileEntries, FILE* image, superblock_t* superblock, int blockSize);
 
 int unloadSuperblock(superblock_t* superblock);
 
@@ -37,25 +38,7 @@ int unloadFileEntries(file_entry_t* fileEntries);
 //========================================================
 
 
-//int openFile(FILE* image, char* img) {
-//    
-//    // Load the image
-//    image = fopen(img, "r+b");
-//    if (image == NULL) {
-//        printf("Error while opening the file\n");
-//        return -1;
-//    }
-//
-//    // Set the pointer at the beginning of the file
-//    fseek(image, 0, SEEK_SET);
-//}
-
 int loadSuperblock(superblock_t* superblock, FILE* image) {
-    
-    // Load the image
-    //FILE* image;
-    //if (openFile(image, img) < 0)
-    //    return -1;
     
     // Get the superblock
     superblock = calloc(1, sizeof(superblock_t));
@@ -63,48 +46,42 @@ int loadSuperblock(superblock_t* superblock, FILE* image) {
         printf("Error while reading file\n");
         return -1;
     }
-
-    // Close the file
-    //fclose(image);
 }
 
 int loadBitmap(unsigned char* bitmap, FILE* image, superblock_t* superblock) {
 
 }
 
-int loadFileEntries(file_entry_t* fileEntries, FILE* image, superblock_t* superblock) {
+int loadFileEntries(file_entry_t* fileEntries, FILE* image, superblock_t* superblock, int blockSize) {
     
     // Load the file entries
     fileEntries = calloc(superblock->fileEntrySize, superblock->nbFileEntries);
     fseek(image, blockSize + superblock->bitmapSize * blockSize, SEEK_SET);
-    fread(arrayFileEntries, sizeof(file_entry_t), superblock->nbFileEntries, image); 
+    fread(fileEntries, sizeof(file_entry_t), superblock->nbFileEntries, image); 
 }
 
 int loadPFS(pfs_t* pfs, char* img) {
+
+    pfs = calloc(sizeof(pfs_t), 1); 
 
     // Load the image
     FILE* image = fopen(img, "r+b");
     if (image == NULL) {
         printf("Error while opening the file");
-        return;
+        return -1;
     }
 
     // Set the pointer at the beginning of the file
     fseek(image, 0, SEEK_SET);
     
     // Get the superblock
-    loadSuperblock(pfs->superblock);
-    //superblock_t* superblock = calloc(1, sizeof(superblock_t));
-    //fread(superblock, sizeof(superblock_t), 1, image);
+    loadSuperblock(&pfs->superblock, image);
 
     // Calculate the size of a block
-    pfs->blockSize = superblock->nbSectorsB * SECTOR_SIZE;
+    pfs->blockSize = pfs->superblock.nbSectorsB * SECTOR_SIZE;
     
     // Load the file entries
-    loadFileEntries(pfs->fileEntries, image, pfs->superblock);
-    //file_entry_t* arrayFileEntries = calloc(superblock->fileEntrySize, superblock->nbFileEntries);
-    //fseek(image, blockSize + superblock->bitmapSize * blockSize, SEEK_SET);
-    //fread(arrayFileEntries, sizeof(file_entry_t), superblock->nbFileEntries, image); 
+    loadFileEntries(pfs->fileEntries, image, &pfs->superblock, pfs->blockSize);
 }
 
 int unloadSuperblock(superblock_t* superblock) {
