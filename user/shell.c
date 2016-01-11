2033212f7454d813f6f6b1a2f474799109e63211
Rudolf Hohn <rudolf.hohn@etu.hesge.ch>
2016-01-11 22:15:35 +0100

    Shell dev ongoing

---
+++ user/shell.c
@@ -0,0 +1,110 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  shell.c
+ *
+ *    Description:  Where the kernel is hidden.
+ *
+ *        Version:  1.0
+ *        Created:  01/11/2016 07:47:28 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Rudolf Hohn, Axel Fahy
+ *   Organization:  HES-SO hepia section ITI
+ *
+ * =====================================================================================
+ */
+
+#include "shell.h"
+
+void emptyBuffer(char* buffer, int size) {
+   for (int i = 0; i < size; i++) {
+       buffer[i] = '\0';
+   }
+}
+
+void split(char* buffer, char* args, int size) {
+    
+    // Parse the buffer
+    int i = 0;
+    while (buffer[i] != ' ') if (++i >= size) break;   
+    
+    // Remove the spaces
+    if (i < size) while (buffer[i] == ' ') buffer[i++] = '\0';
+
+    // Place the args in the args buf
+    for (int j = i; j < size; j++) {
+        buffer[j] = '\0';
+    }
+}
+
+void readBuffer(char* buffer, int size) {
+
+    // Args buffer
+    char args[size];
+    emptyBuffer(&args, size);
+
+    // Split the command and its args
+    split(&buffer, &args, size);
+
+
+}
+
+void shell() {
+    
+    // Init the buffer of CLI and empty it
+    char buffer[BUF_SIZE];
+    emptyBuffer(&buffer, BUF_SIZE);
+    int cursor = 0;
+
+    // Init current character
+    char c = '\0';
+
+    // Infinite loop to manage the CLI
+    for (;;) {
+
+        // Get the current character
+        if (c = getc() < 0)
+            continue;
+
+        // TODO: buffer full
+
+        // Print the character
+        if (putc(c) < 0)
+            printf("Error while printing character");
+
+        // Manage the character locally
+        // ... as a backspace
+        if (c == '\b')
+            buffer[cursor--] = '\0';        // Remove the last character 
+
+        // ... as a return line
+        else if (c == '\n') {
+
+            // Read buffer and launch the command if it is found
+            readBuffer(&buffer, BUF_SIZE);
+
+            // Empty the buffer and reset cursor
+            emptyBuffer(&buffer, BUF_SIZE);
+            cursor = 0;
+        }
+
+        // ... as anything else
+        else 
+            buffer[cursor++] = c;           // Insert the character in buffer
+    }
+}
+
+/* 
+ *
+ * NE PAS IMPLEMENTER ls ICI !!!!!!
+ *
+ * Il doit etre implementer dans un fichier C 
+ * a part et le fichier binaire devra etre inclus dans notre PFS.
+ *
+ *
+ * */
+//void ls() {
+//
+//}
