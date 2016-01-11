b940aace6fc4895ac7e765a16f5ad89e4681e0d7
Rudolf Hohn <rudolf.hohn@etu.hesge.ch>
2016-01-11 19:45:54 +0100

    Start of syscall dev

--- common/syscall_nb.h
+++ common/syscall_nb.h
@@ -2,9 +2,16 @@
 #define _SYSCALL_NB_H_
 
 typedef enum { 
-    SYSCALL_XXX = 0,
-    SYSCALL_YYY,
-    
+    SYSCALL_PUTC = 1,
+    SYSCALL_PUTS = 2,
+    SYSCALL_EXEC = 3,
+    SYSCALL_GETC = 4,
+    SYSCALL_FILE_STAT = 5,
+    SYSCALL_FILE_READ = 6,
+    SYSCALL_FILE_REMOVE = 7,
+    SYSCALL_FILE_ITERATOR = 8,
+    SYSCALL_FILE_NEXT = 9,
+    SYSCALL_GET_TICKS = 10,
     __SYSCALL_END__
 } syscall_t;
 
