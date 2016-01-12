e7131d5ad45cfbb5e138be89496c8462075e35b7
Rudolf Hohn <rudolf.hohn@etu.hesge.ch>
2016-01-12 10:44:51 +0100

    Merging new files for tp4

--- kernel/idt.c
+++ kernel/idt.c
@@ -233,6 +233,9 @@ void idt_init() {
     idt[45] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t)&_irq_13, TYPE_INTERRUPT_GATE, DPL_KERNEL);
     idt[46] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t)&_irq_14, TYPE_INTERRUPT_GATE, DPL_KERNEL);
     idt[47] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t)&_irq_15, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	// IDT entry 48 used for system calls
+	extern void _syscall_handler();  // Implemented in syscall_asm.s
+	idt[48] = idt_build_entry(code_sel, (uint32_t)&_syscall_handler, TYPE_TRAP_GATE, DPL_USER);
 
     idt_ptr.base = (uint32_t)idt; // Base of idt is the first element of idt
 
