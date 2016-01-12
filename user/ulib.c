b2cea12e18886b1943b5762c03947be1897697c1
Axel Fahy <axel@fahy.net>
2016-01-12 09:35:20 +0100

    Include string.h in ulib.c

--- user/ulib.c
+++ user/ulib.c
@@ -32,6 +32,7 @@
  * =====================================================================================
  */
 
+#include "../common/string.h"   // Include string function common to kernel and user
 #include "syscall_nb.h"
 
 extern int syscall(uint32_t nb, uint32_t arg1, uint32_t arg2, uint32_t arg3, utint32_t arg4);
@@ -131,77 +132,6 @@ int strcmp(char* str1, char* str2) {
     else
         return -1;
 }
-
-////////////////////////////////////////////////////////////////////////////////////////
-void itoa(int n, char* s) {
-    char* p = s;
-    char const *digit = "0123456789";
-
-    // Number is negative so we have to print a '-' before
-    if (n < 0) {
-        *(p++) = '-';
-        n *= -1;
-    }
-
-    // If n = 0
-    if (n == 0)
-        p++;
-
-    // Save the size of the array
-    int tmp_n = n;
-    while (tmp_n) {
-        tmp_n = tmp_n / 10;
-        p++;
-    }
-
-    // Adding the last character
-    *p = '\0';
-
-    // Write the integer from the back
-    tmp_n = n;
-    while (tmp_n) {
-        *(--p) = digit[tmp_n%10];
-        tmp_n = tmp_n / 10;
-    }
-
-    // If n = 0
-    if (n == 0)
-        *(--p) = digit[0];
-}
-
-////////////////////////////////////////////////////////////////////////////////////////
-void xtoa(int n, char* s) {
-    char* p = s;
-    char const *hexa = "0123456789ABCDEF";
-
-    // Number is negative so we have to print a '-' before
-    if (n < 0) {
-        *(p++) = '-';
-        n *= -1;
-    }
-
-    // As n is a hex number, we have to print '0x' before
-    *(p++) = '0';
-    *(p++) = 'x';
-
-    // Save the size of the array
-    int tmp_n = n;
-    while (tmp_n) {
-        tmp_n = tmp_n / 16;
-        p++;
-    }
-
-    // Adding the last character
-    *p = '\0';
-
-    // Write the hex from the back
-    tmp_n = n;
-    while (tmp_n) {
-        *(--p) = hexa[tmp_n%16];
-        tmp_n = tmp_n / 16;
-    }
-}
-
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // I/O functions
