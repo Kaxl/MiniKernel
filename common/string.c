e23953be15d76fccd2d082e5505ac5455c903b6c
Axel Fahy <axel@fahy.net>
2016-01-20 16:40:18 +0100

    Reset olf implementation of strcmp

--- common/string.c
+++ common/string.c
@@ -20,22 +20,18 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////
 int strcmp(const char* str1, const char* str2) {
-    for ( ; *str1 == *str2; str1++, str2++)
-	if (*str1 == '\0')
-	    return 0;
-    return ((*(unsigned char *)str1 < *(unsigned char *)str2) ? -1 : +1);
-    //int i = 0;
-    //while (str1[i] == str2[i] && str1[i] != '\0')
-    //    i++;
+    int i = 0;
+    while (str1[i] == str2[i] && str1[i] != '\0')
+        i++;
 
-    //if (str1[i] == str2[i])
-    //    return 0;
-    //else if (str2[i] > str1[i])
-    //    return 1;
-    //else if (str1[i] < str2[i])
-    //    return -1;
-    //else
-    //    return -1;
+    if (str1[i] == str2[i])
+        return 0;
+    else if (str2[i] > str1[i])
+        return 1;
+    else if (str1[i] < str2[i])
+        return -1;
+    else
+        return -1;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
